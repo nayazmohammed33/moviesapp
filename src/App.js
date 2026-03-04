e34f53a83c0d0926e6fd68cac58c90c233eb12cc
Nayaz Mohammed <nayazmohammed93@email.com>
2026-03-04 11:48:44 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-import './App.css';
-import MoviesList from './components/MoviesList';
-import { useState } from 'react';
+import "./App.css";
+import MoviesList from "./components/MoviesList";
+import { useEffect, useState,useCallback } from "react";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [loading,setLoading] =useState(false);
-  const fetchDataHandler = async () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchDataHandler = useCallback((async () => {
     setLoading(true);
-    const response= await fetch("https://swapi.dev/api/films");
-    const data= await response.json(); 
-    setMovies(data.results);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("Something went Wrong!");
+      }
+      const data = await response.json();
+
+      setMovies(data.results);
+    } catch (error) {
+      setError(error.message);
+    }
     setLoading(false);
-  }
+  }),[]);
+
+  useEffect(() => {
+    fetchDataHandler();
+  }, [fetchDataHandler]);
+
   return (
     <div className="App">
       <div className="container">
@@ -20,10 +35,18 @@ function App() {
         </div>
         <div className="movies">
           {/* <MoviesList movies={movies} /> */}
-          {loading &&  <p>Loading movies...  </p>}
-          {!loading && movies.length>0 && movies.map((movie) => (
-              <MoviesList key={movie.episode_id} title={movie.title} openingText={movie.opening_crawl} releaseDate={movie.release_date} />
+          {loading && <p>Loading movies... </p>}
+          {!loading &&
+            movies.length > 0 &&
+            movies.map((movie) => (
+              <MoviesList
+                key={movie.episode_id}
+                title={movie.title}
+                openingText={movie.opening_crawl}
+                releaseDate={movie.release_date}
+              />
             ))}
+          {!loading && error && <p>{error}</p>}
         </div>
       </div>
     </div>
